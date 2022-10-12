a728cb85f6f7905a51e775e0f2ee89ce284dc6bb
John Michael Serminio <johnmichaelserminio@gmail.com>
2022-10-12 15:17:02 +0800

    complete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBF05geOxAMgVocRUZzff225WmZdqE3Egc",
+    authDomain: "clips-d5a1d.firebaseapp.com",
+    projectId: "clips-d5a1d",
+    storageBucket: "clips-d5a1d.appspot.com",
+    appId: "1:716806071293:web:499737368c7e237542af94"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBF05geOxAMgVocRUZzff225WmZdqE3Egc",
+    authDomain: "clips-d5a1d.firebaseapp.com",
+    projectId: "clips-d5a1d",
+    storageBucket: "clips-d5a1d.appspot.com",
+    appId: "1:716806071293:web:499737368c7e237542af94"
+  }
 };
 
 /*
